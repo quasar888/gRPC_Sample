9359046fb0aa4013b5dd3123c4e83f5ccf18d23f
tarin <clement.tarin@gmail.com>
2024-12-11 18:28:21 +0100

    Agregar archivos de proyecto.

---
+++ gRPC_Sample/Services/GreeterService .cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Grpc.Core;
+using Grpc.Net.Client;
+using gRPC_Sample;
+using GrpcServiceSample;
+namespace gRPC_Sample.Services
+{
+    internal class GreeterService: Greeter.GreeterBase
+    {
+        public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(new HelloReply
+            {
+                Message = $"Hello, {request.Name}"
+            });
+        }
+    }
+}
